e358c7e4921e38ff1740953f8e943cd141b882d7
Micheal Limones <micheal@zestylimones.com>
2021-12-28 09:40:19 -0700

    added Annotations to User class.

--- src/main/java/com/technews/model/User.java
+++ src/main/java/com/technews/model/User.java
@@ -1,12 +1,28 @@
 package com.technews.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "user")
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String username;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    @Transient
+    boolean loggedIn;
+
+    private List<Post> posts;
+    private List<Vote> votes;
+    private List<Comment> comments;
+
+
 }
