9fc3b39446eb37da913e0d4b68720ae1b2d9f320
Micheal Limones <micheal@zestylimones.com>
2021-12-27 18:51:15 -0700

    added variable practice.

---
+++ src/main/java/com/technews/VariablePractice.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariablePractice {
+    public static void main(String[] args) {
+        int num = 31;
+        double price = 10.99;
+        double sum = num + price;
+        String name = "Mike";
+        String intro = name + num;
+        System.out.println(sum);
+        System.out.println(intro);
+    }
+}
