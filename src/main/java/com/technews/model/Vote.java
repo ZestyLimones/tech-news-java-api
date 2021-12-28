bb46f344e3567b833b07786be8b0bb7337d919db
Micheal Limones <micheal@zestylimones.com>
2021-12-28 11:25:46 -0700

    added variables to Vote.

--- src/main/java/com/technews/model/Vote.java
+++ src/main/java/com/technews/model/Vote.java
@@ -1,4 +1,17 @@
 package com.technews.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "vote")
+
 public class Vote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer userId;
+    private Integer postId;
 }
