71fc878ddca15f511edbc7319a4581577f0ea978
Micheal Limones <micheal@zestylimones.com>
2021-12-27 18:42:23 -0700

    created variable types project.

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,11 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Cleopatra";
+        int age = 2021 + 69;
+        double price = 19.99;
+        boolean truth = true;
+        boolean falseness = !truth;
+    }
+}
